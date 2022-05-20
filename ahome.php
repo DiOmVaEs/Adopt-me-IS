563d4fcb251790c8b772af8f6688ab08cbdc79a4
Diego Escobedo <dv26143@gmial.com>
2022-05-19 21:17:23 -0500

    19/Mayo

--- ahome.php
+++ ahome.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarsExample09">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active text-info" aria-current="page" href="#">Inicio</a>
+                        <a class="nav-link active text-info" aria-current="page" href="ahome.php">Inicio</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Publicaciones</a>
--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 
     <div class="container-center">
 
-        <form class="form-su form-control form-control-sm" action="" method="post">  
+        <form class="form-su form-control form-control-sm" method="POST" action="php/login-process.php">  
 
             <br><a href="index.html"><img src="images/logo-sm.jpg"></a>
             <h2 class="lead">Inicia Sesion</h2><br>
@@ -25,7 +25,7 @@
                 <input type="password" class="form-control inpustStyle" placeholder="Contraseña" id="password" name="password">
                 <label for="password">Contraseña</label>
             </div>
-            <br><input type="submit" class="btn btn-primary btn-md" colspan="2" value="INGRESAR"><br><br> 
+            <br><input type="submit" class="btn btn-primary btn-md" colspan="2" value="INGRESAR" name="login"><br><br> 
         </form>
     </div>
 
@@ -34,7 +34,7 @@
         <div class="form-su form-control form-control-sm">
             <h1 class="lead">¿No tienes una cuenta?</h1>
             <h2 class="lead">Crea una, solo te toma unos minutos</h2>
-            <a href="signin.html" class="btnindex btn btn-info btn-sm">Registrate</a>
+            <a href="signin.php" class="btnindex btn btn-info btn-sm">Registrate</a>
         </div>
 
     </div>
